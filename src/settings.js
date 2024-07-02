cb26d092686077c1676af4771a2be673cbedc44e
Dpp <dpp666@gmail.com>
2024-07-02 15:12:26 +0700

    1

---
+++ src/settings.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description 是否显示设置右侧面板
+   */
+  showSettings: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description 是否固定表头
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description 是否在侧边栏显示logo
+   */
+  sidebarLogo: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description 是否需要tagsView(多标签)
+   */
+  tagsView: true,
+
+}
---
+++ src/store/getters.js
@@ -0,0 +1,13 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  language: state => state.app.language,
+  size: state => state.app.size,
+  device: state => state.app.device,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name,
+  visitedViews: state => state.tagsView.visitedViews,
+  cachedViews: state => state.tagsView.cachedViews,
+  permission_routes: state => state.permission.routes,
+}
+export default getters
